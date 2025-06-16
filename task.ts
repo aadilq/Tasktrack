a6987c8c202187a5caf0ebb71640c98c3d07c4a4
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-06-16 12:44:47 -0400

    Select input, button, and task list with type assertions

--- task.ts
+++ task.ts
@@ -2,3 +2,9 @@ interface Task{
     id: number,
     text: string
 }
+
+const inputs = document.querySelector('input') as HTMLInputElement
+const button = document.querySelector('button') as HTMLButtonElement
+const tasklist = document.getElementById('task-list') as HTMLElement
+
+let task: Task[] = [];
