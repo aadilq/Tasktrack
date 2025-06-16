f5dcdabfb36a81c02a68d96a3d7bd53cc8594ef3
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-06-16 15:16:46 -0400

    On button click, adds a new task to the list

--- task.ts
+++ task.ts
@@ -8,3 +8,22 @@ const button = document.querySelector('button') as HTMLButtonElement
 const tasklist = document.getElementById('task-list') as HTMLElement
 
 let task: Task[] = [];
+
+
+button.addEventListener("click", function(){
+    const query = inputs.value
+    inputs.value = ""
+
+    if(query.trim() == ""){
+        alert("no value entered!");
+        throw new Error("empty input field error");
+    }
+
+    //Create a new task object
+    const taskObj : Task = {
+        id: Date.now(), 
+        text: query
+    }
+
+
+})
