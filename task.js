68387261fa8919a45ee52d20afdca64d54af0d3a
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-06-17 17:23:38 -0400

    Install typescript dependencies

---
+++ task.js
@@ -0,0 +1,40 @@
+var inputs = document.querySelector('input');
+var button = document.querySelector('button');
+var taskList = document.getElementById('task-list');
+var task = [];
+button.addEventListener("click", function () {
+    var query = inputs.value;
+    inputs.value = "";
+    if (query.trim() == "") {
+        alert("no value entered!");
+        throw new Error("empty input field error");
+    }
+    //Create a new task object
+    var taskObj = {
+        id: Date.now(),
+        text: query
+    };
+    task.push(taskObj);
+});
+function maketodo() {
+    taskList.innerHTML = "";
+    var _loop_1 = function (i) {
+        var _a = task[i], id = _a.id, text = _a.text;
+        var element = document.createElement('div');
+        element.innerHTML = "\n            <span class=\"task\">".concat(text, "</span>\n            <span class=\"delete\">\n                <i class=\"fa-solid fa-trash\"></i>\n            </span>\n        ");
+        // Handle the delete button click event
+        var deleteButton = document.querySelector('.delete');
+        deleteButton.addEventListener("click", function () {
+            // Remove the task from the array based on its ID
+            var filteredarray = task.filter(function (taskobj) { return taskobj.id !== id; });
+            task = filteredarray;
+            localStorage.setItem("task array", JSON.stringify(task));
+            taskList.removeChild(element);
+        });
+        element.classList.add('todo');
+        taskList.appendChild(element);
+    };
+    for (var i = 0; i <= task.length; i++) {
+        _loop_1(i);
+    }
+}
